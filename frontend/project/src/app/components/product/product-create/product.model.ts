9455d5007c6ebcdd125203a422ffce94d70a9c6e
rayennecandido <cursosrayenne@gmail.com>
2023-05-16 09:13:21 -0300

    components do produto

--- frontend/project/src/app/components/product/product-create/product.model.ts
+++ frontend/project/src/app/components/product/product-create/product.model.ts
@@ -1,14 +1,14 @@
 export interface Product {
     id?: any
-    name: String
-    CPF: String
-    email: String
-    telefone: String
-    dataNascimento: String
-    tipo: String
-    endereco: String
-    numero: String
-    cidade: String
-    UF: String
-    CEP: String
+    name: any
+    CPF?: String
+    email?: String
+    telefone?: String
+    dataNascimento?: String
+    tipo?: String
+    endereco?: String
+    numero?: String
+    cidade?: String
+    UF?: String
+    CEP?: String
     }
